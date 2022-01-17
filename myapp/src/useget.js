2277b3ef603691c85e556ac7880157b5bba4e633
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-17 17:51:20 -0300

    c10- new file useget

---
+++ myapp/src/useget.js
@@ -0,0 +1,50 @@
+import { useReducer, useEffect } from "react"
+import axios from "axios"
+
+const reducer = (state, action) => {
+    // I go handling my state!!
+    // how use reducer ?
+    if(action.type === 'REQUEST'){
+       return {
+         ...state ,
+         laoding : true
+       }
+    }
+    if(action.type ==='SUCCESS'){
+      return { 
+        ...state ,
+        loading: false ,
+        data: action.data
+      }
+    }
+   
+     return state
+   }
+   
+   //create hoock personalies
+   const UseGet = Url => {
+      // try it flag loading
+      // i can seeing this data have conexitons
+      //flag reducer
+   const [data, dispatch] = useReducer (reducer, {
+     loading: true,
+     data: {}
+   })
+   //remember i have uses useEffect to caught in the api's because without inside out i dont getting caught
+   useEffect(() => {
+     dispatch({type: 'REQUEST'})
+      axios
+     .get(Url)
+     .then(res => {
+       dispatch({type:'SUCCESS', data: res.data})
+     }) 
+   }, [])
+      return data
+   }
+
+
+
+
+
+
+export default UseGet
