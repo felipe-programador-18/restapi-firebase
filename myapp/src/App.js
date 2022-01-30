43d0cc7b3639bdd6cdcdbc7806b111f175c1a525
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-30 20:13:51 -0300

    c34- end rearrange hoocks

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,28 +1,24 @@
 import React  from 'react'
 import './App.css'
-import useDelete from './useDelete'
-//import UseGet from './useget'
-import UsePost from './UsePost'
 
 // THIS REST turned about init
 import REST from './rest'
 const BaseUrl ='https://build-api-8a54e-default-rtdb.firebaseio.com/'
 
-const {UseGet} = REST(BaseUrl)
+const {UseGet,UsePost, useDelete} = REST(BaseUrl)
 
-const Url = 'https://build-api-8a54e-default-rtdb.firebaseio.com/movimentacao/2022-01/despesa.json'
 
 function App() {
   const  data = UseGet ('movimentacao/2022-01')
 
-const [postData, post] =  UsePost(Url)
+const [postData, post] =  UsePost('movimentacao/2022-01')
 const [Deletdata, remove] = useDelete()
 // i have delete way whole
 
 
  const Doremove = () => {
   // i have delete way whole
-   remove('https://build-api-8a54e-default-rtdb.firebaseio.com/movimentacao/2022-01/despesa.json/-Muh4Yr2PlJiZWVmSNpi')
+   remove('movimentacao/2022-01/-MuhE8AHOBl46uBwFRHX')
  }
 
   const Savenovo = () =>{
--- myapp/src/rest.js
+++ myapp/src/rest.js
@@ -3,6 +3,12 @@
 import { useReducer, useEffect } from "react";
 import axios from "axios";
 
+const INITIAL_STATE = {
+  loading: false, 
+  data: {}
+}
+
+
 const reducer = (state, action) => {
     // I go handling my state!!
     // how use reducer ?
@@ -30,10 +36,7 @@ const reducer = (state, action) => {
       // try it flag loading
       // i can seeing this data have conexitons
       //flag reducer
-      const [data, dispatch] = useReducer (reducer, {
-       loading: true,
-       data: {}
-       })
+      const [data, dispatch] = useReducer (reducer,INITIAL_STATE)
    //remember i have uses useEffect to caught in the api's because without inside out i dont getting caught
       useEffect(() => {
        dispatch({type: 'REQUEST'})
@@ -45,9 +48,42 @@ const reducer = (state, action) => {
       }, [])
       return data
     }  
+
+    const UsePost = resource => {
+      const [data, dispatch] = useReducer (reducer, INITIAL_STATE)
+
+      const post  = data => {
+       dispatch({type: 'REQUEST'})    
+      axios
+      .post(BaseUrl + resource + '.json', data)
+      .then(res => {
+          dispatch({type:'SUCCESS', data: res.data})
+      })
+      }
+    return [data, post]
+  }
+
+  const useDelete = () => {
+    // remember here i can set reducer!!!
+    const [data, dispatch] = useReducer (reducer, INITIAL_STATE)
+    const remove  = resource => {
+     dispatch({type: 'REQUEST'})    
+    axios
+    .delete(BaseUrl + resource + '.json')
+    .then(() => {
+        dispatch({type:'SUCCESS'})
+    })
+    }
+  return [data, remove]
+}
+
+
+
     //here it is retunr useget that stay inside of function init!!
     return {
-      UseGet
+      UseGet,
+      UsePost,
+      useDelete
     }
    }
    // here i am caught kind url to getting make rearrange
