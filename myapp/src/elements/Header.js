19e464cbd40dd8daf8fcb1157230b3456ea57e68
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-03 17:12:19 -0300

    c47- adding new file and create new element

---
+++ myapp/src/elements/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+//this moment am build new element
+
+<nav className='navbar navbar-light bg-primary'>
+   <div className='container'>
+     <a className='navbar-brand '> Project my money, working firebase with restapi and React!!!</a>
+   </div>
+</nav>
+
