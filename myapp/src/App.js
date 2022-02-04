6f30696fd00a66489e3fd0f9ff6db11adad0feba
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-04 17:13:44 -0300

    c64- remove somethigs that wrote

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,22 +1,21 @@
 import React  from 'react'
-import './App.css'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import './App.css' 
 import Header from './elements/Header'
 import Month from './Meses'
 import AddingMo from './Adding.mon' 
 
 // THIS REST turned about init
-import REST from './rest'
-const BaseUrl ='https://build-api-8a54e-default-rtdb.firebaseio.com/'
+//import REST from './rest'
+//const BaseUrl ='https://build-api-8a54e-default-rtdb.firebaseio.com/'
 
-const {UseGet,UsePost, useDelete} = REST(BaseUrl)
+//const {UseGet,UsePost, useDelete} = REST(BaseUrl)
 
 
 function App() {
                   
- 
-
-
   return (
+    <Router>
     <div>
     <Header/>
     <div className='container'> 
@@ -26,6 +25,7 @@ function App() {
     
    </div>
     </div>
+    </Router>
 );
   }
 
