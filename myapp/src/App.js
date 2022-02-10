3331c59ff336f6c90d47993a6ac131eba02b3921
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-10 10:46:23 -0300

    c67- adding new something

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,39 +1,35 @@
 import React  from 'react'
 import {BrowserRouter as Router,Routes , Route} from 'react-router-dom'
 import './App.css' 
-import Header from './elements/Header'
 import Month from './Meses'
 import AddingMo from './Adding.mon' 
+import Outro from './elements/Header'
 
 // THIS REST turned about init
 //import REST from './rest'
 //const BaseUrl ='https://build-api-8a54e-default-rtdb.firebaseio.com/'
 
 //const {UseGet,UsePost, useDelete} = REST(BaseUrl)
-const Home =  () =>{
-  
+const Home =() =>{
   return  <div className='container'> 
-   
     <AddingMo/>
     <Month/>
     
    </div>
 }
 
-const Together  = () =>{
-  return <h1><Header/></h1>
+const Together= () =>{
+  return <h1><Outro/> </h1>
 }
 
 function App() {
                   
   return (
     <Router>
-    <Routes>
-        <Header/>
-        <Route path='/elements/Header' exact element= {<Together/>} />
+      <Routes>
+      <Route path='/elements/Header' exact element= {<Together/>} />
         <Route path="/" exact element={<Home />} />
-         
-    </Routes>
+     </Routes>
   </Router>
 );
   }
