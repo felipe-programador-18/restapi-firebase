ca6fc48a9312059af93f70ced3ab703fb2302e04
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-10 13:37:04 -0300

    c69- adding new to folder to rearrange

---
+++ myapp/src/pages/Home/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Month from '../../Meses'
+import AddingMo from '../../Adding.mon' 
+
+//theorical i have import all that have been inside Home!!!
+// this code stay more structure
+
+const Home =() =>{
+    return <div className='container'> 
+    <AddingMo/>
+      <Month/>
+      
+     </div>
+   
+  }
+
+
+export default Home  
---
+++ myapp/src/pages/Movimentacoes.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+// create new movimentacoes to adding in the another router!!
+const Movimentacoes = () =>{
+    return <h1>Movimentacoes</h1>
+  }
+
+
+export default Movimentacoes
