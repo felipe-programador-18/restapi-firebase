61cd7477ea3c22228f66ec13463c43ea6b3ee197
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-10 13:42:42 -0300

    c73- import new file to another folder

---
+++ myapp/src/pages/Home/Adding.mon.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+const AddingMo = () => {
+
+ return (
+     <React.Fragment>
+<h2>Adding month</h2>
+
+ <select>
+ <option value='2019'>2019</option>
+ <option value='2020'>2020</option>
+ <option value='2021'>2021</option>
+ </select>
+
+ <select>
+ <option value='01'>01</option>
+ <option value='02'>02</option>
+ <option value='03'>03</option>
+ </select>
+ <button> adding month</button>
+ </React.Fragment>
+ 
+
+ )
+}
+
+export default AddingMo
