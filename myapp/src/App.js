78bb3ae55e81127e66b96aa03c661f3ef1bb9ff9
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-10 13:37:33 -0300

    c70- modify somethi

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,37 +1,36 @@
 import React  from 'react'
 import {BrowserRouter as Router,Routes , Route} from 'react-router-dom'
 import './App.css' 
-import Month from './Meses'
-import AddingMo from './Adding.mon' 
 import Header from './elements/Header'
+import Home from './pages/Home'
+import Movimentacoes from './pages/Movimentacoes'
 
 // THIS REST turned about init
 //import REST from './rest'
 //const BaseUrl ='https://build-api-8a54e-default-rtdb.firebaseio.com/'
 
 //const {UseGet,UsePost, useDelete} = REST(BaseUrl)
-const Home =() =>{
-  return  <div className='container'> 
-    <AddingMo/>
-    <Month/>
-    
-   </div>
-}
+
 
 const Together= () =>{
   return <h1><Header/> </h1>
        
 }
 
+
 function App() {
                   
   return (
+    <React.Fragment>
+    <Header/>
     <Router>
       <Routes>
-         <Route path='' exact element ={<Together/>} />
          <Route path="/" exact element={<Home />} />
+         <Route path='/together' exact element= {<Together />} />
+         <Route path ='/movimentacoes/:data' exact element= {<Movimentacoes/>} />
      </Routes>
   </Router>
+  </React.Fragment>
 );
   }
 
