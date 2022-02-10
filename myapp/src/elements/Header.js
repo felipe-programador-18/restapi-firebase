9e39e606e645e989ac8b44b72968996ff19f7fb0
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-10 10:46:45 -0300

    c68- try it seeing resolve issues

--- myapp/src/elements/Header.js
+++ myapp/src/elements/Header.js
@@ -3,18 +3,18 @@ import React from "react"
 
 //this moment am build new element
 
-const Header = () =>{
+const Outro = () =>{
 
   return( 
-    
+      <React.Fragment> 
       <nav className='navbar navbar-light bg-primary'>
         <div className='container'>
            <a className='navbar-brand '> Project my money, working firebase with restapi and React!!!</a>
        </div>
       </nav>
-    
+      </React.Fragment>
   )
 }
 
 
-export default Header
+export default Outro
