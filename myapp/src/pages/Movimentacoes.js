30c4d830a11e3de7b0f8ee5d31400083ca3ab8bc
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-11 18:04:05 -0300

    c89-adding new modify

--- myapp/src/pages/Movimentacoes.js
+++ myapp/src/pages/Movimentacoes.js
@@ -8,13 +8,38 @@ const { UseGet } = REST(BaseUrl)
 // create new movimentacoes to adding in the another router!!
 const Movimentacoes = ({ match }) =>{
     // now i going flag in the date
-    const data = UseGet(match.params.data)
-
+    const data = UseGet(`movimentacoes/${match.params.data}`)
+    
     return  (
         <div>
         <h1>Movimentacoes</h1>
         <pre> {JSON.stringify(data)} </pre>
-      
+        <table  className="table" >
+        <thead>
+         <tr>
+         <th>Descrição</th>
+         <th>Valor</th>
+         </tr>
+
+        </thead>
+        <tbody>
+         {
+            Object
+            .keys(data.data)
+            .map(movimentacao => {
+                return (
+                    <tr>
+                      <td>{data.data[movimentacao].descricao}</td>
+                      <td>{data.data[movimentacao].valor}</td>
+                    </tr>
+                )
+            })
+       
+         }
+        </tbody>
+
+        </table>
+
         </div>
         )
 }
