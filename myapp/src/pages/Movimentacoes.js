6d51ccea52b3b5da0993891be401b9b04b547a34
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-21 17:15:30 -0300

    c101- adding new somethings

--- myapp/src/pages/Movimentacoes.js
+++ myapp/src/pages/Movimentacoes.js
@@ -1,5 +1,6 @@
 import React from "react"
 import REST from '../utils/rest'
+import { useState } from "react"
 
 const BaseUrl ='https://build-api-8a54e-default-rtdb.firebaseio.com/'
 const { UseGet } = REST(BaseUrl)
@@ -10,6 +11,21 @@ const { UseGet } = REST(BaseUrl)
 // create new movimentacoes to adding in the another router!!
 const Movimentacoes = ({match}) =>{
     // now i going flag in the date
+
+    // controller form is name called to change formulating with HOOCK
+    // here i 'm target  value of input in the state, because i have using hookc state!!
+    const [ descricao, setdescricao] = useState('')
+    const [valor, setvalor] = useState(0.0)
+    
+    // this function to get date of input that went create, thought about that !!!
+    const OnchangeDescricao = evt => {
+      setdescricao(evt.target.value)
+    }
+
+    const OnchangeValor = evt => {
+       setvalor(parseFloat(evt.target.valor))
+    }
+    
     const data = UseGet( `movimentacoes/${ match.params.data}`)
      return  (
         <div className='container'>
@@ -41,6 +57,14 @@ const Movimentacoes = ({match}) =>{
                    )
                 })
             }
+
+            <tr>
+            
+            <td> <input type='text' value={descricao}  onChange={OnchangeDescricao} /> </td>
+            <td> <input type='text'value={valor} onChange={OnchangeValor} />  </td>
+            
+            </tr>
+
          </tbody>
         </table>
          
